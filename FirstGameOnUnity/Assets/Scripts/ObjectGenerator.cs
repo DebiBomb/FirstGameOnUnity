bb9005de0560788fdb393680d75fb4ee289ab48c
De <matteodebia05@gmail.com>
2022-12-25 19:22:53 +0100

    25/12/2022
    Correzione dello script ObjectGenerator

--- FirstGameOnUnity/Assets/Scripts/ObjectGenerator.cs
+++ FirstGameOnUnity/Assets/Scripts/ObjectGenerator.cs
@@ -4,25 +4,73 @@ using UnityEngine;
 
 public class ObjectGenerator : MonoBehaviour
 {
+    // intervallo di tempo tra la generazione di ogni oggetto
+    public float timeInterval = 1;  // in secondi
+
+    // distanza minima tra oggetti generati
+    public float minDistance = 10;
+
+    // prefab dell'oggetto da generare
     public GameObject objectPrefab;
-    public float generationRate = 1.0f;
-    public float xRange = 10.0f;
-    public Camera mainCamera;
 
-    void Update()
+    // lista di oggetti generati
+    private List<GameObject> objects = new List<GameObject>();
+
+    void Start()
     {
-        // Genera un nuovo oggetto ogni generationRate secondi
-        if (Time.timeSinceLevelLoad % generationRate < Time.deltaTime)
+        // avvia il loop di generazione degli oggetti
+        StartCoroutine(GenerateObjects());
+    }
+
+    IEnumerator GenerateObjects()
+    {
+        while (true)  // esegui il loop all'infinito
         {
-            // Scegli una posizione casuale all'interno del range di generazione
-            float xPos = Random.Range(-xRange, xRange);
-            float yPos = Random.Range(-12, -8);
+            // calcola le dimensioni del range di spazio in base alla posizione della main camera
+            Vector3 bottomLeft = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 0));
+            Vector3 topRight = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, 0));
 
-            // Ottieni la posizione corrente della main camera
-            Vector3 cameraPos = mainCamera.transform.position;
+            float minX = bottomLeft.x;
+            float maxX = topRight.x;
+            float minY = bottomLeft.y;
+            float maxY = topRight.y;
 
-            // Crea una nuova istanza dell'oggetto prefab alla posizione scelta
-            Instantiate(objectPrefab, new Vector2(cameraPos.x + xPos, cameraPos.y + yPos), Quaternion.identity);
+            // genera una nuova posizione casuale per l'oggetto
+            float x = Random.Range(minX, maxX);
+            float y = Random.Range(minY-12, maxY-8);
+
+            // verifica che la posizione x sia maggiore o minore di almeno 3 rispetto all'ultimo oggetto generato
+            bool validPosition = true;
+            if (objects.Count > 0)
+            {
+                float lastX = objects[objects.Count - 1].transform.position.x;
+                if (Mathf.Abs(x - lastX) < 3)
+                {
+                    validPosition = false;
+                }
+            }
+
+            // verifica se la posizione è troppo vicina ad un oggetto già generato
+            bool tooClose = false;
+            foreach (GameObject obj in objects)
+            {
+                float distance = Vector2.Distance(obj.transform.position, new Vector2(x, y));
+                if (distance < minDistance)
+                {
+                    tooClose = true;
+                    break;
+                }
+            }
+
+            // se la posizione è valida, crea l'oggetto e aggiungilo alla lista
+            if (validPosition && !tooClose)
+            {
+                GameObject obj = Instantiate(objectPrefab, new Vector2(x, y), Quaternion.identity);
+                objects.Add(obj);
+            }
+
+        // attendi l'intervallo di tempo prima di generare un nuovo oggetto
+        yield return new WaitForSeconds(timeInterval);
         }
     }
 }
---
+++ FirstGameOnUnity/Assets/Scripts/ObjectGenerator1.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectGenerator1 : MonoBehaviour
+{
+    public GameObject objectPrefab;
+    public float generationRate = 1.0f;
+    public float xRange = 10.0f;
+    public Camera mainCamera;
+
+    void Update()
+    {
+        // Genera un nuovo oggetto ogni generationRate secondi
+        if (Time.timeSinceLevelLoad % generationRate < Time.deltaTime)
+        {
+            // Scegli una posizione casuale all'interno del range di generazione
+            float xPos = Random.Range(-xRange, xRange);
+            float yPos = Random.Range(-12, -8);
+
+            // Ottieni la posizione corrente della main camera
+            Vector3 cameraPos = mainCamera.transform.position;
+
+            // Crea una nuova istanza dell'oggetto prefab alla posizione scelta
+            Instantiate(objectPrefab, new Vector2(cameraPos.x + xPos, cameraPos.y + yPos), Quaternion.identity);
+        }
+    }
+}
